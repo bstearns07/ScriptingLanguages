81ddd30f729ebb18e6d46d998b018943703dde34
bstearns07 <bstearns07@gmail.com>
2025-09-07 12:50:49 -0500

    3.4.1 edit

--- Week2/Lab3-4-1_AutomationScript_BenStearns.py
+++ Week2/Lab3-4-1_AutomationScript_BenStearns.py
@@ -7,32 +7,30 @@
 ########################################################################################
 
 #import needed modules
-from pathlib import Path
 import os
 import shutil
 
 # define needed variables
 files = ["data1.txt", "data2.txt", "data3.txt"]
 folder = "BenStearns_prog3_4_scripting"
-destination = Path(folder)
 num = 1 # to write different data to each file
 
 # if folder doesn't exist, make one
-if not os.path.exists(destination):
-    os.mkdir(destination)
+if not os.path.exists(folder):
+    os.mkdir(folder)
 
 # loop through files in list
 for filename in files:
     # create a Path object for the file's location to check if it already exists
-    destinationToCheck = destination / filename # appends the file location to the parent folder
+    destinationToCheck = os.path.join(folder, filename) # append the file location to the parent folder
 
     # only proceed creating the file if it doesn't already exist in the folder
-    if not destinationToCheck.exists():
+    if not os.path.exists(destinationToCheck):
         # create the file and write data to it
         with open(filename, "w") as file:
             file.write(f"Data number {num}\n")
         # move the file from the parent folder to the destination folder
-        shutil.move(filename, destination)
+        shutil.move(filename, folder)
 
     # increment num variable just to write something different in the next file
     num += 1
