737852a0982db5f1f37918e2b4c66fa43d7bcc63
HazelKaliati <151781643+HazelKaliati@users.noreply.github.com>
2025-09-16 15:12:18 -0500

    5.6.1

--- Week_5/Lab5-6-1_TCPClient/TCPClient_NetSim_BenStearns.py
+++ Week_5/Lab5-6-1_TCPClient/TCPClient_NetSim_BenStearns.py
@@ -7,24 +7,43 @@
 ############################################################################################################
 
 import socket # used for basic network communication and port scanning
+import time
 
-# define IP address and port for the listening network
+# define IP address and port information for the listening network
 IP = "127.0.0.1"
 PORT = 7001
+currentAttemptNumber = 1
 
-# creates a context manager so the socket closes once out of scope
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # set the address family to IPv4 and type to TCP
-    # if operations take longer than 3 seconds, timeout and try again only once more
-    # connect to the network
-    s.settimeout(3.0)
-    s.connect((IP, PORT))
+# try the connection only a maximum of 2 times
+while currentAttemptNumber < 3:
+    try:
+        # use "with" statement so the socket closes once out of scope
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # set the address family to IPv4 and type to TCP
+            # set a timeout of 3 seconds and create the connection using the IP address and port information
+            s.settimeout(3.0)
+            s.connect((IP, PORT))
 
-    # send the required commands to the server one at a time
-    for command in ['SET Course networking','GET Course', 'DEL networking', 'GET Course']:
-        s.sendall((command + "\n").encode("utf-8")) # encodes the command string into bytes required for network communication
+            # send the required commands to the server one at a time
+            for command in ['SET Course networking\n','GET Course\n', 'DEL networking\n', 'GET Course\n', 'QUIT\n']:
+                s.sendall(command.encode("utf-8")) # encodes the command string into bytes required for network communication
 
-        # store a maximum of 1024 bytes from the server response
-        # print the response by decoding the bytes received back into a string
-        # skip any bytes received that aren't utf-8 to avoid decoding errors
-        data = s.recv(1024)
-        print("Server response: " + data.decode("utf-8",errors="ignore"))
+                # store the server response up to a maximum of 1024 bytes
+                # print the response by decoding the bytes received back into a string
+                # skip any bytes received that aren't a valid utf-8 format to avoid decoding errors
+                data = s.recv(1024)
+                print("Server response: " + data.decode("utf-8",errors="ignore"))
+            # break the while loop once all commands have been executed
+            break
+    # catch any timeout exceptions.
+    # On first attempt display a "trying again" message. Otherwise, display "ending program" message
+    except socket.timeout:
+        if currentAttemptNumber == 1:
+            print(f"Attempt {currentAttemptNumber} timed out. Attempting to re-connect...")
+            currentAttemptNumber += 1
+        else:
+            print(f"Attempt {currentAttemptNumber} timed out. Ending program...")
+            currentAttemptNumber += 1
+    #catch all other exceptions and print what error happened. then break from the while loop
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        break
