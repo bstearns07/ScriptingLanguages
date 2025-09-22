a8d08c4ae3a4810ddf823d0de6ddfbc5739f80ae
bstearns07 <bstearns07@gmail.com>
2025-09-21 20:21:34 -0500

    practice

--- Practice/io.py
+++ Practice/io.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 path = "C:/Users/bstea/OneDrive/Desktop/secret.txt"
 path2 = "C:/Users/bstea/OneDrive/Desktop/SecretStuff"
@@ -23,3 +24,28 @@ for file_name in os.listdir(path2):
     # Copy only files (skip directories)
     if os.path.isfile(src):
         shutil.copy(src, dst)
+
+# define a function that copies all files in a directory to another
+def Copy_Files(src: str, dest: str):
+    # cast to Path object
+    src = Path(src)
+    dst = Path(dest)
+
+    # check directory exists
+    if not src.exists():
+        raise FileNotFoundError("Directory not found")
+
+    # create destination and all necessary parent folders. Dont raise exception if already exists
+    dst.mkdir(parents=True,exist_ok=True)
+
+    # loop through src directory's files
+    for file in src.rglob("*"):
+        # remove the parent "fluff" from the destination path
+        dstFilePath = dst / file.relative_to(src)
+        # if file is a directory, make it in the destination
+        if file.is_dir():
+            dstFilePath.mkdir(parents=True,exist_ok=True)
+        else:
+            if dstFilePath.exists():
+                print(f"WARNING. About to overwrite{file.name}")
+            shutil.copy(file,dstFilePath)
