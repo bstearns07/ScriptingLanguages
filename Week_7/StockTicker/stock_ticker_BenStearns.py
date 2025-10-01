945431ad27f1ace633e923d1b37da0f76567061b
bstearns07 <bstearns07@gmail.com>
2025-10-01 14:57:58 -0500

    updated so only 5 rows of data print and logging info is silenced

--- Week_7/StockTicker/stock_ticker_BenStearns.py
+++ Week_7/StockTicker/stock_ticker_BenStearns.py
@@ -3,20 +3,32 @@
 # Author.....: Ben Stearns
 # Date.......: 10-1-2025
 # Description: The purpose of this program is to:
-#                 - Ask the user for a stock ticker symbol
-#                 - Retrieve and print the first 5 rows of data for the user's ticker symbol
-#                 - Display a line chart of the closing price
+#                 - Ask the user for any number of stock ticker symbols
+#                 - Retrieve and print the first 5 rows of data for the user's ticker symbol(s)
+#                 - Display a line chart of the closing prices for each ticker symbol
 ############################################################################################################
 
 # imports
 import requests
 from bs4 import BeautifulSoup
-import yfinance as yf
-import matplotlib
+import yfinance as yf # to retrieve finance data
+import matplotlib # to plot data into a chart
+import logging # to access and silence any unnecessary logging information printed to the console by yfinance
 
 # get user input
-user_input = input("Enter any number stock ticker symbols: ")
-ticker_symbols = yf.Ticker(user_input)
-input_as_list = user_input.split(' ')
+user_input = input("Enter any number stock ticker symbols: ").split(' ')
 print(f"Retrieving data for {user_input}...\n")
-print(yf.download(input_as_list, auto_adjust=False))
+
+# Silence yfinance logging
+logging.getLogger("yfinance").setLevel(logging.CRITICAL)
+
+try:
+    # download the data for the user's ticker symbols
+    data = yf.download(user_input, auto_adjust=False, progress=False).head(5)
+    # check if returned data frame is empty. Only print the data if it has rows in it
+    if data.empty:
+        print(f"No data was found for ticker symbol(s): {user_input}")
+    else:
+        print(data)
+except Exception as e:
+    print(f"Error: {e}")
