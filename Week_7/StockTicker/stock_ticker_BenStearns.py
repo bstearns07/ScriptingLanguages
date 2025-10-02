d13e6e82252ed51b78d8b5491428b9c15a3ad4fd
bstearns07 <bstearns07@gmail.com>
2025-10-01 20:36:39 -0500

    got chart working

--- Week_7/StockTicker/stock_ticker_BenStearns.py
+++ Week_7/StockTicker/stock_ticker_BenStearns.py
@@ -12,23 +12,38 @@
 import requests
 from bs4 import BeautifulSoup
 import yfinance as yf # to retrieve finance data
-import matplotlib # to plot data into a chart
+import matplotlib.pyplot as plt # to plot data into a chart
 import logging # to access and silence any unnecessary logging information printed to the console by yfinance
 
-# get user input
-user_input = input("Enter any number stock ticker symbols: ").split(' ')
-print(f"Retrieving data for {user_input}...\n")
+# get user input as a list in all uppercase
+tickers = input("Enter any number stock ticker symbols: ").upper().split(' ')
+print(f"Retrieving data for {tickers}...\n")
 
 # Silence yfinance logging
 logging.getLogger("yfinance").setLevel(logging.CRITICAL)
 
 try:
     # download the data for the user's ticker symbols
-    data = yf.download(user_input, auto_adjust=False, progress=False).head(5)
+    data = yf.download(tickers, auto_adjust=False, progress=False).head(5)
     # check if returned data frame is empty. Only print the data if it has rows in it
     if data.empty:
-        print(f"No data was found for ticker symbol(s): {user_input}")
+        print(f"No data was found for ticker symbol(s): {tickers}")
     else:
         print(data)
+        # Plot the closing numbers
+        # data.index = dates on the x-axis
+        # plt.plot(x-axis data, y data, format_string to control color & line style(optional), key arguments(opt)
+        plt.figure(figsize=(8,5))
+        for ticker in tickers:
+            plt.plot(data.index, data["Close"][ticker], label=ticker)
+
+        # Add labels, title, and legend
+        plt.xlabel("Date")
+        plt.ylabel("Closing Price (USD)")
+        plt.title("Closing Prices")
+        plt.legend()
+
+        # Show the chart
+        plt.show()
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"An error occured: {e}")
