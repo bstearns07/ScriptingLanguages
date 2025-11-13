27a6e8e38eab86ba85f86bc1a8cd6fa85fbc5483
HazelKaliati <151781643+HazelKaliati@users.noreply.github.com>
2025-11-13 11:20:21 -0600

    added db records

--- YugiohCardDigitizer/tesseract.py
+++ YugiohCardDigitizer/tesseract.py
@@ -19,6 +19,7 @@ import DBcm
 from preprossing import preprocess_image    # for use of the preprocess_image() function
 from extractor import extract_text          # for use of the extract_text() function
 from utils import extract_card_info      # for use of the extract_contact_info() function
+from Yugioh_Card import YugiohCard
 
 images_dir = "samples"
 db_details ="Cards.sqlite3"
@@ -40,6 +41,46 @@ def main():
         print()
 
         with DBcm.UseDatabase(db_details) as db:
+            SQL = """create table if not exists cards (
+                    id integer not null primary key autoincrement,
+                    name varchar(32) not null,
+                    attack integer,
+                    defense integer,
+                    description varchar(500) not null,
+                    type varchar(32),
+                    color varchar(32) not null      
+                )"""
+            db.execute(SQL)
+            blue_eyes = YugiohCard(
+                name="Blue-Eyes White Dragon",
+                description="This legendary dragon is a powerful engine of destruction.",
+                attack=3000,
+                defense=2500,
+                type="Dragon",
+                color="Light"
+            )
+            dark_magician = YugiohCard(
+                name="Dark Magician",
+                description = "The ultimate wizard in terms of attack and defense.",
+                attack=2500,
+                defense=2000,
+                type="Spellcaster",
+                color="Yellow"
+            )
+            insert_SQL = """
+                INSERT INTO cards (name, attack, defense, description, type, color)
+                VALUES (?, ?, ?, ?, ?, ?)
+            """
+            db.execute(insert_SQL, (
+                dark_magician.name,
+                dark_magician.attack,
+                dark_magician.defense,
+                dark_magician.description,
+                dark_magician.type,
+                dark_magician.color
+            ))
+            db.execute("SELECT * FROM cards")
+            print(f"Cards in table: {db.fetchall()}")
             db.execute("pragma table_list")
             results = db.fetchall() #returns most recent command executed
             print(f"Results: {results}")
